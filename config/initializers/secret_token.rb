460727cde46a4285fd2444b211fd2a6f46e45ecf
soupdawgie <soupdawgie@gmail.com>
2012-03-18 13:01:28 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notepad::Application.config.secret_token = '5c1f155c5c31d849fd5d84d00929c74e73a4db31ac7dc591f04f276bc68d30d3f6834539974ac9afc5dd46cc0db41256a4ddec7c40758443d64b4f1b5b826db2'
